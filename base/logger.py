aed1fefc866d8ddd363a49679398bda5af9252b5
LeoProko <leoproko126@gmail.com>
2022-01-12 08:07:06 +0000

    fix logger path

--- base/logger.py
+++ base/logger.py
@@ -8,5 +8,5 @@ logging.basicConfig(format=FORMAT)
 Logger = logging.getLogger("dex_trading_system")
 Logger.setLevel(LEVEL)
 
-Logger.addHandler(logging.FileHandler("dex_trading_system/debug.log"))
+Logger.addHandler(logging.FileHandler("debug.log"))
 # Logger.propagate = False
