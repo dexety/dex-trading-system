8555f9f241bbc7e46c066ff4ef7fbb53c077b932
Lev Rybakov <lirybakov@edu.hse.ru>
2022-01-16 13:15:45 +0300

    edit readme + correct swap code

--- research/lr-0002-liquidity-pool-leaps/scripts/swap_some_dai.py
+++ research/lr-0002-liquidity-pool-leaps/scripts/swap_some_dai.py
@@ -1,4 +1,4 @@
-from brownie import interface, network, config, SwapExamples
+from brownie import interface, network, config, SwapExamples, accounts
 from scripts.helpful_scripts import get_account
 
 
@@ -8,16 +8,24 @@ def main():
     dai = interface.ERC20(addresses["dai_token"])
     account = get_account(type="test")
 
-    print("Deploing swapper...")
-    swap = SwapExamples.deploy(
-        addresses["uniswap_swap_router"], {"from": account}
-    )
-    print("Done")
+    print("Getting swapper contract...")
+    if len(SwapExamples) > 0:
+        swap = SwapExamples[-1]
+    else:
+        print("Swapper has not been deployed yet")
+        print("Deploing swapper...")
+        swap = SwapExamples.deploy(
+            addresses["uniswap_swap_router"], {"from": account}
+        )
+        print("Done")
 
-    print("Approving...")
-    dai.approve(swap, amountIn, {"from": account})
-    print("Done")
+    print("Checking allowance...")
+    if dai.allowance(account, swap) < amountIn:
+        print("Allowance not enough")
+        print("Approving spending of $10...")
+        dai.approve(swap, amountIn, {"from": account})
+        print("Done")
 
-    print("Swap initiated...")
-    swap.swapExactInputSingle(amountIn)
+    print("Initiating swap...")
+    swap.swapExactInputSingle(amountIn, {"from": account})
     print("Done")
