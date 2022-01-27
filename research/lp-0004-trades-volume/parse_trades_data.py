734ea106b496ce8bbb1a43610272b2cd19c30636
Lev Rybakov <lirybakov@edu.hse.ru>
2022-01-27 20:04:05 +0300

    unfinished work

--- research/lp-0004-trades-volume/parse_trades_data.py
+++ research/lp-0004-trades-volume/parse_trades_data.py
@@ -6,115 +6,20 @@ from datetime import datetime, timedelta
 import numpy as np
 from tqdm import tqdm
 
+sys.path.append("../../")
 
-class WindowIndicators:
-    @staticmethod
-    def get_all_indicators():
-        return {
-            "trade-amount": lambda window: WindowIndicators.trade_amount(
-                window
-            ),
-            "trade-volume": lambda window: WindowIndicators.trade_volume(
-                window
-            ),
-            "open-close-diff": lambda window: WindowIndicators.open_close_diff(
-                window
-            ),
-            "moving-average": lambda window: WindowIndicators.moving_average(
-                window
-            ),
-            "weighted-moving-average": (
-                lambda window: WindowIndicators.weighted_moving_average(window)
-            ),
-            "exp-moving-average": (
-                lambda window: WindowIndicators.exp_moving_average(window)
-            ),
-            "stochastic-oscillator": (
-                lambda window: WindowIndicators.stochastic_oscillator(window)
-            ),
-        }
-
-    @staticmethod
-    def exp_moving_average(window, alpha=0.5) -> float:
-        if not window:
-            return 0
-        ema = float(window[0]["price"])
-        for i in range(1, len(window)):
-            ema = ema + alpha * (float(window[i]["price"]) - ema)
-        return ema
-
-    @staticmethod
-    def trade_amount(window) -> float:
-        return len(window)
-
-    @staticmethod
-    def trade_volume(window) -> float:
-        if not window:
-            return 0
-        return sum(map(lambda trade: float(trade["size"]), window))
-
-    @staticmethod
-    def open_close_diff(window) -> float:
-        if not window:
-            return 0
-        return float(window[-1]["price"]) / float(window[0]["price"])
-
-    @staticmethod
-    def moving_average(window) -> float:
-        if not window:
-            return 0
-        return sum(map(lambda trade: float(trade["price"]), window)) / len(
-            window
-        )
-
-    @staticmethod
-    def weighted_moving_average(window) -> float:
-        if not window:
-            return 0
-        return (
-            sum(
-                map(
-                    lambda trade: float(trade["price"]) * float(trade["size"]),
-                    window,
-                )
-            )
-            / sum(map(lambda trade: float(trade["size"]), window))
-        )
-
-    @staticmethod
-    def stochastic_oscillator(window) -> float:
-        if not window:
-            return 0
-        return (
-            float(window[-1]["price"])
-            - float(
-                min(window, key=lambda trade: float(trade["price"]))["price"]
-            )
-        ) / (
-            max(
-                1,
-                float(
-                    max(window, key=lambda trade: float(trade["price"]))[
-                        "price"
-                    ]
-                )
-                - float(
-                    min(window, key=lambda trade: float(trade["price"]))[
-                        "price"
-                    ]
-                ),
-            )
-        )
-
+from utils.indicators.indicators import Indicators
+from utils.buy_sell_queue.buy_sell_queue import BuySellWindow
+from utils.helpful_scripts import string_to_datetime
 
 class DataParser:
     punch_threashold = 0.0002
-    trades_window_slices_sec = [600, 60, 30, 10, 5, 1]
-    trades_window_sec = 600
-    punch_window_sec = 30
+    trade_window_slices_sec = [600, 60, 30, 10, 5, 1]
+    trade_window_td = timedelta(seconds=600)
+    punch_window_td = timedelta(seconds=30)
     random_data_pc = 0.005
     SIDES = ["BUY", "SELL"]
-    indicator_functions = WindowIndicators.get_all_indicators()
+    indicator_functions = Indicators.get_all_indicators()
 
     def __init__(
         self,
@@ -130,149 +35,71 @@ class DataParser:
         )
         self.input_path = input_path
         self.output_path = output_path
-        self.trades_window = {"BUY": deque(), "SELL": deque()}
-        self.punch_window = {"BUY": deque(), "SELL": deque()}
         self.output_data = []
         self.progress_bar = tqdm(range(self.data_it_max - self.data_it))
 
         self.init_windows()
 
     def init_windows(self) -> None:
-        self.set_trades_window()
-        self.set_punch_window()
-
-    def reset_windows_after_punch(self) -> None:
-        punch_window_head_time = self.get_min_item_time_from_window(
-            self.punch_window
+        first_trade_dt = string_to_datetime(
+            self.data[self.data_it]["createdAt"]
         )
-        while self.get_min_item_time_from_window(
-            self.punch_window
-        ) - punch_window_head_time < timedelta(seconds=self.punch_window_sec):
-            self.update_punch_window()
+        self.trade_window = BuySellWindow(self.trade_window_td)
+        self.punch_window = BuySellWindow(self.punch_window_td)
+        self.set_window_borders(first_trade_dt)
+        self.fill_trade_window()
+        self.fill_punch_window()
 
-    @staticmethod
-    def get_datetime(string_time: str) -> datetime:
-        return datetime.strptime(string_time, "%Y-%m-%dT%H:%M:%S.%fZ")
+    def set_window_borders(self, trade_window_from_dt: datetime) -> None:
+        self.trade_window.set_window_borders(trade_window_from_dt)
+        self.punch_window.set_window_borders(
+            trade_window_from_dt + self.trade_window_td
+        )
+
+    def update_windows_after_punch(self) -> None:
+        self.set_window_borders(
+            self.trade_window.from_dt + self.punch_window_td
+        )
+        self.fill_punch_window()
+        self.clean_trade_window()
+        self.move_from_punch_window_to_trade_window()
 
     def get_new_trade(self) -> dict:
         self.progress_bar.update()
-        if self.data_it < self.data_it_max:
-            trade = self.data[self.data_it]
-            self.data_it += 1
-            return trade
-        return None
+        trade = self.data[self.data_it]
+        self.data_it += 1
+        return trade
 
-    def get_max_item_from_window(self, window: dict) -> dict:
-        if window["BUY"] and window["SELL"]:
-            if self.get_datetime(
-                window["BUY"][-1]["createdAt"]
-            ) > self.get_datetime(window["SELL"][-1]["createdAt"]):
-                return window["BUY"][-1]
-            else:
-                return window["SELL"][-1]
-        if window["BUY"]:
-            return window["BUY"][-1]
-        if window["SELL"]:
-            return window["SELL"][-1]
-        return None
+    def clean_trade_window(self) -> None:
+        while not self.trade_window.trade_inside(
+            self.trade_window.common_queue[0]
+        ):
+            self.trade_window.pop_front()
 
-    def get_max_item_time_from_window(self, window: dict) -> datetime:
-        trade = self.get_max_item_from_window(window)
-        if trade:
-            return self.get_datetime(trade["createdAt"])
-        return None
+    def fill_trade_window(self) -> None:
+        while self.trade_window.trade_inside(self.data[self.data_it]):
+            self.trade_window.push_back(self.get_new_trade())
 
-    def get_min_item_from_window(self, window: dict) -> dict:
-        if window["BUY"] and window["SELL"]:
-            if self.get_datetime(
-                window["BUY"][0]["createdAt"]
-            ) < self.get_datetime(window["SELL"][0]["createdAt"]):
-                return window["BUY"][0]
-            else:
-                return window["SELL"][0]
-        if window["BUY"]:
-            return window["BUY"][0]
-        if window["SELL"]:
-            return window["SELL"][0]
-        return None
+    def move_from_punch_window_to_trade_window(self) -> None:
+        while not self.punch_window.trade_inside(
+            self.punch_window.common_queue[0]
+        ):
+            self.trade_window.push_back(self.punch_window.pop_front())
 
-    def get_min_item_time_from_window(self, window: dict) -> datetime:
-        trade = self.get_min_item_from_window(window)
-        if trade:
-            return self.get_datetime(trade["createdAt"])
-        return None
+    def fill_punch_window(self) -> None:
+        while self.punch_window.trade_inside(self.data[self.data_it]):
+            self.punch_window.push_back(self.get_new_trade())
 
-    def is_window_empty(self, window: dict) -> bool:
-        return not (
-            self.get_max_item_time_from_window(window)
-            or self.get_max_item_time_from_window(window)
+    def update_windows_no_punch(self) -> None:
+        new_trade = self.get_new_trade()
+        self.set_window_borders(
+            string_to_datetime(new_trade["createdAt"])
+            - self.punch_window_td
+            - self.trade_window_td
         )
-
-    def set_trades_window(self) -> None:
-        while self.is_window_empty(
-            self.trades_window
-        ) or self.get_max_item_time_from_window(
-            self.trades_window
-        ) - self.get_min_item_time_from_window(
-            self.trades_window
-        ) <= timedelta(
-            seconds=self.trades_window_sec
-        ):
-            new_trade = self.get_new_trade()
-            if not new_trade:
-                return
-            self.trades_window[new_trade["side"]].append(new_trade)
-
-    def set_punch_window(self) -> None:
-        while self.is_window_empty(
-            self.punch_window
-        ) or self.get_max_item_time_from_window(
-            self.punch_window
-        ) - self.get_min_item_time_from_window(
-            self.punch_window
-        ) <= timedelta(
-            seconds=self.punch_window_sec
-        ):
-            new_trade = self.get_new_trade()
-            if not new_trade:
-                return
-            self.punch_window[new_trade["side"]].append(new_trade)
-
-    def update_punch_window(self) -> None:
-        if self.get_min_item_from_window(self.punch_window):
-            self.trades_window[
-                self.get_min_item_from_window(self.punch_window)["side"]
-            ].append(self.get_min_item_from_window(self.punch_window))
-            self.punch_window[
-                self.get_min_item_from_window(self.punch_window)["side"]
-            ].popleft()
-        while self.data_it < self.data_it_max and (
-            self.is_window_empty(self.punch_window)
-            or (
-                self.get_max_item_time_from_window(self.punch_window)
-                - self.get_min_item_time_from_window(self.punch_window)
-            )
-            < timedelta(seconds=self.punch_window_sec)
-        ):
-            new_trade = self.get_new_trade()
-            if not new_trade:
-                return
-            self.punch_window[new_trade["side"]].append(new_trade)
-
-    def update_trades_window(self) -> None:
-        for side in self.SIDES:
-            while (self.trades_window[side]) and self.get_datetime(
-                self.trades_window[side][-1]["createdAt"]
-            ) - self.get_datetime(
-                self.trades_window[side][0]["createdAt"]
-            ) > timedelta(
-                seconds=self.trades_window_sec
-            ):
-                self.trades_window[side].popleft()
-
-    def update_windows(self) -> None:
-        self.update_punch_window()
-        self.update_trades_window()
+        self.punch_window.push_back(new_trade)
+        self.clean_trade_window()
+        self.move_from_punch_window_to_trade_window()
 
     def get_max_price(self, window: dict, side: str) -> float:
         return float(
@@ -305,27 +132,25 @@ class DataParser:
         for side in self.SIDES:
             for n_trades_ago in [1, 10, 50, 100, 1000]:
                 diff = (
-                    self.get_datetime(self.data[self.data_it]["createdAt"])
-                    - self.get_datetime(
-                        self.data[max(0, self.data_it - n_trades_ago)][
-                            "createdAt"
-                        ]
+                    string_to_datetime(
+                        self.trade_window.side_queues[side][-1]["createdAt"]
+                    )
+                    - string_to_datetime(
+                        self.trade_window.side_queues[side][
+                            max(
+                                0,
+                                len(self.trade_window.common_queue)
+                                - n_trades_ago
+                                - 1,
+                            )
+                        ]["createdAt"]
                     )
                 ).total_seconds()
                 update[
                     "seconds-since-" + str(n_trades_ago) + "-trades-ago-" + side
                 ] = diff
-            for window_slice_sec in self.trades_window_slices_sec:
-                window_slice = list(
-                    filter(
-                        lambda trade: self.get_datetime(
-                            self.trades_window[side][-1]["createdAt"]
-                        )
-                        - self.get_datetime(trade["createdAt"])
-                        <= timedelta(seconds=window_slice_sec),
-                        self.trades_window[side],
-                    )
-                )
+            for window_slice_sec in self.trade_window_slices_sec:
+                window_slice = self.trade_window.get_side_queue_slice(side, window_slice_sec)
 
                 for indicator_name in self.indicator_functions:
                     column_name = (
@@ -343,29 +168,29 @@ class DataParser:
     def calculate_punches(self, update, side) -> None:
         if side == "BUY" and self.punch_window["SELL"]:
             update[
-                "punch-" + side + "-" + str(self.punch_window_sec) + "-sec"
+                "punch-" + side + "-" + str(self.punch_window_td) + "-sec"
             ] = max(
                 0,
                 1
                 - self.get_max_price(self.punch_window, "SELL")
-                / float(self.trades_window["BUY"][-1]["price"]),
+                / float(self.trade_window["BUY"][-1]["price"]),
             )
         elif side == "SELL" and self.punch_window["BUY"]:
             update[
-                "punch-" + side + "-" + str(self.punch_window_sec) + "-sec"
+                "punch-" + side + "-" + str(self.punch_window_td) + "-sec"
             ] = min(
                 0,
                 1
                 - self.get_min_price(self.punch_window, "BUY")
-                / float(self.trades_window["SELL"][-1]["price"]),
+                / float(self.trade_window["SELL"][-1]["price"]),
             )
         else:
             update[
-                "punch-" + side + "-" + str(self.punch_window_sec) + "-sec"
+                "punch-" + side + "-" + str(self.punch_window_td) + "-sec"
             ] = 0
 
     def add_result(self) -> None:
-        if not self.trades_window["BUY"] or not self.trades_window["SELL"]:
+        if not self.trade_window["BUY"] or not self.trade_window["SELL"]:
             return
 
         update = {}
@@ -376,7 +201,7 @@ class DataParser:
             max_punch = max(
                 max_punch,
                 update[
-                    "punch-" + side + "-" + str(self.punch_window_sec) + "-sec"
+                    "punch-" + side + "-" + str(self.punch_window_td) + "-sec"
                 ],
                 key=abs,
             )
@@ -385,7 +210,7 @@ class DataParser:
             abs(max_punch) > self.punch_threashold
             or np.random.random() < self.random_data_pc
         ):
-            current_trade_dt = self.get_datetime(
+            current_trade_dt = string_to_datetime(
                 self.data[self.data_it]["createdAt"]
             )
             midnight = current_trade_dt.replace(
@@ -397,7 +222,7 @@ class DataParser:
 
             self.calculate_indicators(update)
 
-            current_trade_dt = self.get_datetime(
+            current_trade_dt = string_to_datetime(
                 self.data[self.data_it]["createdAt"]
             )
             midnight = current_trade_dt.replace(
@@ -413,7 +238,7 @@ class DataParser:
                 self.data[self.data_it]["createdAt"]
             )
             self.output_data.append(update)
-            self.reset_windows_after_punch()
+            self.update_windows_after_punch()
 
     def write_data(self) -> None:
         field_names = list(self.output_data[0].keys())
--- utils/buy_sell_queue/buy_sell_queue.py
+++ utils/buy_sell_queue/buy_sell_queue.py
@@ -1,16 +1,47 @@
 from collections import deque
 from datetime import datetime, timedelta
 
-class BuySellQueue:
-    buy_queue = deque()
-    sell_queue = deque()
+from utils.helpful_scripts import string_to_datetime
 
-    @staticmethod
-    def get_datetime(string_time: str) -> datetime:
-        return datetime.strptime(string_time, "%Y-%m-%dT%H:%M:%S.%fZ")
+class BuySellWindow:
+    side_queues = {"BUY": deque(), "SELL": deque()}
+    common_queue = deque()
 
-    def reset(self) -> None:
-        self.buy_queue = deque()
-        self.sell_queue = deque()
+    from_dt = datetime()
+    to_dt = datetime()
+    window_interval_td = timedelta()
 
-    def add_
+    def __init__(self, window_interval_td: timedelta) -> None:
+        self.window_interval_td = window_interval_td
+
+    def set_window_borders(self, from_dt: datetime) -> None:
+        self.from_dt = from_dt
+        self.to_dt = self.from_dt + self.window_interval_td
+
+    def get_last_to_first_td(self) -> timedelta:
+        return string_to_datetime(self.common_queue[-1]) - string_to_datetime(self.common_queue[0])
+    
+    def trade_inside(self, trade: dict) -> bool:
+        trade_time = string_to_datetime(trade["createdAt"])
+        return trade_time >= self.from_dt and trade_time <= self.to_dt
+
+    def pop_front(self) -> dict:
+        first_trade = self.common_queue.popleft()
+        self.side_queues[first_trade["side"]].popleft()
+        return first_trade
+
+    def push_back(self, trade: dict) -> None:
+        self.common_queue.append(trade)
+        self.side_queues[trade["side"]].append(trade)
+
+    def get_side_queue_slice(self, side: str, seconds: int) -> list:
+        if seconds == self.window_interval_td.seconds:
+            return self.side_queues[side]
+        
+        slice = []
+        i = 1
+        while i <= len(self.side_queues[side]) and string_to_datetime(self.side_queues[side][-i]["createdAt"]) >= self.to_dt - timedelta(seconds=seconds):
+            slice.append(self.side_queues[side][-i])
+            i += 1
+        
+        return slice
