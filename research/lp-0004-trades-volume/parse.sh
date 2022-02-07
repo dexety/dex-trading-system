5aa42f99f0e7eca58634390f2b87ed66062a3ecd
Lev Rybakov <leo.rybakov@icloud.com>
2022-02-07 23:57:17 +0300

    resolve

--- research/lp-0004-trades-volume/parse.sh
+++ research/lp-0004-trades-volume/parse.sh
@@ -1,7 +1,9 @@
-# python3 parse/split_trades_file.py 3
+python3 parse/split_trades_file.py 5
 
 python3 parse/parse_trades_data.py 0
 python3 parse/parse_trades_data.py 1
 python3 parse/parse_trades_data.py 2
+python3 parse/parse_trades_data.py 3
+python3 parse/parse_trades_data.py 4
 
-python3 parse/concatenate.py 3
+python3 parse/concatenate.py 5
