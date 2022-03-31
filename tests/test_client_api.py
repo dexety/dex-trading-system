ee1f84c9d03e9d4d837b11f3cff8ab3275c5b8cd
Lev Rybakov <leo.rybakov@icloud.com>
2022-04-01 00:02:07 +0300

    fix test

--- tests/test_client_api.py
+++ tests/test_client_api.py
@@ -69,7 +69,7 @@ def test_get_order_book():
 @safe_execute
 def test_send_and_cancel_limit_order():
     order = dydx_connector.send_limit_order(
-        symbol=MARKET_ETH_USD, side=ORDER_SIDE_BUY, price=1, quantity=0.1
+        symbol=MARKET_ETH_USD, side=ORDER_SIDE_BUY, price="1", quantity="0.1"
     )
     assert order != {}
     cancel_order = dydx_connector.cancel_order(order["order"]["id"])
