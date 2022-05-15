43c0016aa5dd8af44c3e812b790c0a46635c3fdf
LeoProko <leoproko126@gmail.com>
2022-05-15 15:45:00 +0300

    fix

--- strategy/arbitrage/run_trader.py
+++ strategy/arbitrage/run_trader.py
@@ -1,16 +1,16 @@
 import trader
-from dydx3.constants import MARKET_BTC_USD
+from dydx3.constants import MARKET_ETH_USD
 
 if __name__ == "__main__":
     trader_settings = trader.Settings(
         trailing_percent=0.005,
-        quantity=0.001,
+        quantity=0.01,
         profit_threshold=0.0015,
         sec_to_wait=30,
         sec_after_trade=0,
         signal_threshold=0.003,
         dydx_market=MARKET_BTC_USD,
-        binance_market="BTCUSD_PERP",
+        binance_market="ETHUSD_PERP",
     )
     trader = trader.Trader(trader_settings)
     trader.run()
